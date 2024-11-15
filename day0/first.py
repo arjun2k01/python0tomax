a8b63777b528e3f836a4ae024e766b62167f0c37
arjun2k01 <122463451+arjun2k01@users.noreply.github.com>
2024-11-15 14:45:57 +0530

    journey begins here

---
+++ day0/first.py
@@ -0,0 +1,4 @@
+print('hello world');
+# module and pip introduction
+#module is a file containing a set of functions, classes, and variables that can be imported into other programs. just like a node modules in JavaScript.examples flask, django, and pandas are some popular modules.
+#pip is a package manager for Python that allows you to install and manage software packages written in Python.just like npm pnpm in JavaScript.
---
+++ day0/module.py
@@ -0,0 +1,12 @@
+import pyjokes
+joke=pyjokes.get_joke()
+print(joke)
+
+# python can be used as a calculator
+# repl is read-evaluate-print loop
+# to run the code in the repl, type the code and press enter
+# multiline comments can """ or ''' be used for multi-line comments"
+'''
+practise set 1:
+write a program to print twinkle twinkle little star poem in python
+'''
